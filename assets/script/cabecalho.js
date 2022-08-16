f19cbacde9ee4991990499019266a2a529d497c1
LuKeDSP <lucasfpilatti@gmail.com>
2022-08-16 14:50:08 -0300

    Update

--- assets/script/cabecalho.js
+++ assets/script/cabecalho.js
@@ -4,17 +4,22 @@ var projectBtn = window.document.getElementById('projectsbtn');
 var contactBtn = window.document.getElementById('contactmebtn');
 
 function start() {
-    window.document.getElementById('startbtn').onclick = document.getElementById('presentation').scrollIntoView();
+    window.document.getElementById('startbtn').onclick = window.document.getElementById('presentation').scrollIntoView();
 }
 
 function about() {
-    window.document.getElementById('aboutbtn').onclick = document.getElementById('about').scrollIntoView();
+    window.document.getElementById('aboutbtn').onclick = window.document.getElementById('about').scrollIntoView();
 }
 
 function project() {
-    window.document.getElementById('projectsbtn').onclick = document.getElementById('project').scrollIntoView();
+    window.document.getElementById('projectsbtn').onclick = window.document.getElementById('project').scrollIntoView();
+}
+
+function contact() {
+    window.document.getElementById('contacBtn').onclick = window.document.getElementById('contact').scrollIntoView();
 }
 
 startBtn.addEventListener("click", start);
 aboutBtn.addEventListener("click", about);
 projectBtn.addEventListener("click", project);
+contactBtn.addEventListener("click", contact);
