b08a2a6b13f3c1f9b28171f02436da65539ae844
LuKeDSP <lucasfpilatti@gmail.com>
2022-08-16 21:45:17 -0300

    Update

---
+++ assets/script/contato.js
@@ -0,0 +1,28 @@
+var insta = window.document.getElementById('txtig');
+var face = window.document.getElementById('txtfb');
+var gh = window.document.getElementById('txtgh');
+var linked = window.document.getElementById('txtli');
+var whats = window.document.getElementById('txtwpp');
+var email = window.document.getElementById('txtemail');
+var downloadcv = window.document.getElementById('txtcv');
+
+function instagram() {
+    window.document.getElementById('txtig').onclick = window.open("https://www.instagram.com/luke_pilatti/", "_blank");
+}
+
+function facebook() {
+    window.document.getElementById('txtfb').onclick = window.open("https://www.facebook.com/lucas.francapilatti", "_blank");
+}
+
+function github() {
+    window.document.getElementById('txtgh').onclick = window.open("https://github.com/LuKeDSP", "_blank");
+}
+
+function linkedin() {
+    window.document.getElementById('txtli').onclick = window.open("https://www.linkedin.com/in/lucas-pilatti-4957841bb/", "_blank");
+}
+
+insta.addEventListener("click", instagram);
+face.addEventListener("click", facebook);
+gh.addEventListener("click", github);
+linked.addEventListener("click", linkedin);
